2655aa96fa475b8a2c25ce185d97848a058a27ab
MD Jannatin Naim <mail.jnaim@gmail.com>
2021-11-14 20:56:25 +0600

    Clean up code.

--- siadcon.sh
+++ siadcon.sh
@@ -2,39 +2,40 @@
 
 cat "MOTD"
 
-
 if [ -z $1 ]
 then
         echo './siadcon.sh <list of domains>'
         exit 1
 fi
-echo 'Looking for Subdomains>>>'
+
+echo 'Looking for sub-domains... >>>'
 
 while read line
 do
-        for var in $line
-        do
-                echo 'Enumerating:' $var
+    for var in $line
+    do
+        echo 'Enumerating:' $var
 
-                subfinder -silent -d $var > out1
-                cat out1 >> subs1
+        subfinder -silent -d $var > out1
+        cat out1 >> subs1
 
-                assetfinder -subs-only $var > out2
-                cat out2 >> subs2
+        assetfinder -subs-only $var > out2
+        cat out2 >> subs2
 
-                rm out1 out2
-        done
+        rm out1 out2
+    done
 done < $1
 
 sort -u subs1 subs2 > all_subs
 rm subs1 subs2
+
 echo 'Saved subdomains to all_subs'
 
-echo 'Finding live hosts>>>'
+echo 'Finding live hosts...'
 
 cat all_subs | httprobe > live_subs
 echo 'Saved live hosts to live_subs'
-echo 'Cheking for subdomain takeover>>>'
+echo 'Cheking for subdomain takeover...'
 
 subjack -w all_subs -a
 
